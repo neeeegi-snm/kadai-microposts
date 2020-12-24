72182738ff6d06934fe889d17c62eebc1f363ab6
m.neg <mngit.tawa20@gmail.com>
2020-12-24 13:16:21 +0000

    post

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,9 @@
 module SessionsHelper
-  def current_user
+  def current_user #現在ログインしているユーザを取得して代入
     @current_user ||= User.find_by(id: session[:user_id])
   end
   
-  def logged_in?
+  def logged_in? #ログインしているかを調べてtrueかfalseを返す
     !!current_user
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(1..25).each do |number|
+  user = User.find(3)
+  micropost = user.microposts.build(content: 'test content ' + number.to_s)
+  micropost.save
+end
